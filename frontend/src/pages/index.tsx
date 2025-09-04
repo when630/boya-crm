74d011f821ee079f57fa584cba97269923ed6133
when630 <jel063033@gmail.com>
2025-09-04 10:14:13 +0900

    Revert "Initial Commit"
    This reverts commit 8e2f507b12f58d4d9014b89c381281446a2b85fc.

--- frontend/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: { destination: "/trials", permanent: false },
-  };
-};
-
-export default function Index() { return null; }
